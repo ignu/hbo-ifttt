a20658976c81b26fc91d2ac1e9fb88ac586e3418
Len Smith <len@barrison.com>
2017-05-11 12:49:40 -0700

    Status - Valid Request is passing

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+const API_PREFIX = '/ifttt/v1/'
+
+app.get(`${API_PREFIX}status`, function (req, res) {
+  console.log('req', req)
+  res.send({});
+})
+
+app.post('/status', function (req, res) {
+  console.log('reaaaq', req)
+  res.send({});
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
